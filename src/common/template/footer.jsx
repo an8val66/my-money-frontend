ecbee37e1a420bb3bc1fb1786dcf14c6d20bbca4
Anderson Borges <andersonvaleriano.borges@gmail.com>
2019-01-05 08:20:10 -0200

    Component Footer

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2019
+            <a href='https://github.com/an8val66/my-money-frontend' target='_blank'> - Github.</a>
+        </strong>
+    </footer>
+)
