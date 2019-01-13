d78f871e4dd661a9fb73106e62020d4c193e0b7a
Anderson Borges <andersonvaleriano.borges@gmail.com>
2019-01-13 16:51:57 -0200

    Atualização das bibliotecas

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom'
 import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
 
+import * as serviceWorker from './serviceWorker'
 import promise from 'redux-promise'
 import multi from 'redux-multi'
 import thunk from 'redux-thunk'
 
-import Routes from './main/routes'
+import AuthOrApp from './main/authOrApp'
 import reducers from './main/reducers'
 
 
@@ -16,7 +17,12 @@ const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devT
 
 ReactDOM.render(
     <Provider store={store}>
-        <Routes />
+        <AuthOrApp />
     </Provider>
     , document.getElementById('app')
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register();
